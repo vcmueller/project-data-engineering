a89aaf2f049ad5421a21ec1f86cefd1aa7b20028
vcmueller <vanessa.cmueller@icloud.com>
2022-04-17 17:38:58 +0200

    Update visualization pages

--- code/visualization/visualization-process.py
+++ code/visualization/visualization-process.py
@@ -1,6 +1,5 @@
 import pandas
 from sqlalchemy import create_engine
-import psycopg2
 from bokeh.plotting import figure, output_file, show
 from bokeh.tile_providers import CARTODBPOSITRON, get_provider
 from bokeh.io import show
@@ -13,6 +12,7 @@ from dateutil.relativedelta import relativedelta
 from prefect import task, Flow, Parameter
 from prefect.schedules import IntervalSchedule
 from prefect.executors import LocalDaskExecutor #DaskExecutor
+import dbstatus
 
 '''
 def logStatus(status, message):
@@ -39,19 +39,14 @@ def logStatus(status, message):
 '''
 
 @task(max_retries=3, retry_delay=timedelta(seconds=1))
-def getResult():
+def getMLResult():
     
-    # Wait for ML process to complete
-    while (checkStatus()):
-        print("Not ready -- wait ...")
-        time.sleep(120)
-
-    alchemyEngine = create_engine('postgresql+psycopg2://etl:etl@db/db', pool_recycle=3600);
+    # Open connection to the database
+    alchemyEngine = create_engine(f'postgresql+psycopg2://{dbconfig.USER}:{dbconfig.PASSWORD}@{dbconfig.HOST}/{dbconfig.DBNAME}', pool_recycle=3600);
     connection = alchemyEngine.connect();
     
     # Get the date of the last temperature loaded
     sql = """select * from temperature_level ;"""
-    #data = pandas.read_sql(sql, connection)
     data = pandas.read_sql_query(sql,con=connection)
 
     # Close database connection
@@ -69,17 +64,49 @@ def getResult():
         ]
     data_table = DataTable(source=source, columns=columns, width=800, height=800)
 
-    output_file("pages/result.html")
+    # Output file with results
+    output_file("pages/mlresult.html")
+    show(data_table)
+
+@task
+def getETLResult():
+    
+    # Open connection to the database
+    alchemyEngine = create_engine(f'postgresql+psycopg2://{dbconfig.USER}:{dbconfig.PASSWORD}@{dbconfig.HOST}/{dbconfig.DBNAME}', pool_recycle=3600);
+    connection = alchemyEngine.connect();
+    
+    # Get the date of the last temperature loaded
+    sql = """select * from temperatures ;"""
+    data = pandas.read_sql_query(sql,con=connection)
+
+    # Close database connection
+    connection.close()
+
+    source = ColumnDataSource(data)
+
+    columns = [
+            TableColumn(field="region", title="Region"),
+            TableColumn(field="country", title="Country"),
+            TableColumn(field="city", title="City"),
+            TableColumn(field="quarter", title="Quarter"),
+            TableColumn(field="date", title="Date"),
+            TableColumn(field="avgtemp", title="Temperature"),
+        ]
+    data_table = DataTable(source=source, columns=columns, width=800, height=800)
+
+    # Output file with results
+    output_file("pages/etlresult.html")
     show(data_table)
 
 @task
 def getStatus():
-    alchemyEngine = create_engine('postgresql+psycopg2://etl:etl@db/db', pool_recycle=3600);
+
+    # Open connection to the database
+    alchemyEngine = create_engine(f'postgresql+psycopg2://{dbconfig.USER}:{dbconfig.PASSWORD}@{dbconfig.HOST}/{dbconfig.DBNAME}', pool_recycle=3600);
     connection = alchemyEngine.connect();
     
     # Get the date of the last temperature loaded
     sql = """select * from status ;"""
-    #data = pandas.read_sql(sql, connection)
     data = pandas.read_sql_query(sql,con=connection)
 
     # Close database connection
@@ -96,40 +123,28 @@ def getStatus():
         ]
     data_table = DataTable(source=source, columns=columns, width=800, height=800)
 
-    output_file("pages/etlstatus.html")
+    output_file("pages/status.html")
     show(data_table)
 
-def checkStatus():
-    ''''
-    Get last status logged
-    '''
-    # Open connection to the database
-    connection = psycopg2.connect(f"host='{dbconfig.HOST}' dbname='{dbconfig.DBNAME}' user='{dbconfig.USER}' password='{dbconfig.PASSWORD}'")
-    cursor = connection.cursor()
-    
-    # Get the date of the last temperature loaded
-    cursor.execute("select status from status order by id desc limit 1;")
-    lastStatus = cursor.fetchone()[0]
-    connection.close()
-    
-    # If it's the first run, set a initial date
-    if lastStatus == 4:
-        return False
-    else:
-        return True
 
 def main():
     
     # Set Prefect scheduler to run every month
     schedule = IntervalSchedule(
         start_date=datetime.utcnow() + timedelta(seconds=1),
-        interval=timedelta(minutes=5),
+        interval=timedelta(minutes=2),
     )
 
     # Configure Prefect flow
     with Flow("etl", schedule=schedule) as flow:
-        getResult()
+        getMLResult()
+        getETLResult()
         getStatus()
+
+    # Wait for ML process to complete
+    while (dbstatus.checkStatus(4)):
+        print("Waiting for ETL and ML to finish...")
+        time.sleep(60)
 
     # Execute ETL flow
     flow.run(executor=LocalDaskExecutor())#DaskExecutor())
